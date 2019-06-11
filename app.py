5243b37d4f4d0c218d50619a499b1db2662c7508
SohnYongJin <winiel@smfcast.com>
2019-06-11 10:21:34 +0900

    init

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+# def hello_world():
+#     return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
